08a06e935f8d232c3c8a7b03c3fba2fe378dccab
gogotanaka <mail@tanakakazuki.com>
2014-10-28 16:08:06 -0700

    Item#create -> Item.create

--- lib/freee/item.rb
+++ lib/freee/item.rb
@@ -1,14 +1,16 @@
 module Freee
   class Item < Freee::Base
-    def create
-      Freee.client.post('/api/1/items', :item, **kwargs)
-    end
+    class << self
+      def create
+        Freee.client.post('/api/1/items', :item, **kwargs)
+      end
 
-    def self.list(company_id)
-      Freee.client.get(
-        "/api/1/items?company_id=#{company_id.to_i}",
-        :item
-      )
+      def list(company_id)
+        Freee.client.get(
+          "/api/1/items?company_id=#{company_id.to_i}",
+          :item
+        )
+      end
     end
   end
 end
