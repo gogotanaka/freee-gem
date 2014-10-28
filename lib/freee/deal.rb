46a02af4bcf6c4aedcd42a31e7d7513436cfa71b
gogotanaka <mail@tanakakazuki.com>
2014-10-28 16:07:25 -0700

    Deal#create -> Deal.create

--- lib/freee/deal.rb
+++ lib/freee/deal.rb
@@ -1,25 +1,27 @@
 module Freee
   class Deal < Freee::Base
-    def create
-      Freeee.client.post('/api/1/deals', :deals, **kwargs)
-    end
+    class << self
+      def create
+        Freeee.client.post('/api/1/deals', :deals, **kwargs)
+      end
 
-    def self.list(company_id, **kwargs)
-      params = Freee.encode_params(kwargs)
-      Freee.client.get(
-        "/api/1/deals?company_id=#{company_id.to_i}#{params}",
-        :deal
-      )
-    end
+      def list(company_id, **kwargs)
+        params = Freee.encode_params(kwargs)
+        Freee.client.get(
+          "/api/1/deals?company_id=#{company_id.to_i}#{params}",
+          :deal
+        )
+      end
 
-    def self.list_income(company_id, **kwargs)
-      kwargs[:type] = 'income'
-      self.list(company_id, **kwargs)
-    end
+      def list_income(company_id, **kwargs)
+        kwargs[:type] = 'income'
+        self.list(company_id, **kwargs)
+      end
 
-    def self.list_expense(company_id, **kwargs)
-      kwargs[:type] = 'expense'
-      self.list(company_id, **kwargs)
+      def list_expense(company_id, **kwargs)
+        kwargs[:type] = 'expense'
+        self.list(company_id, **kwargs)
+      end
     end
   end
 end
